0b3c015265eb9db5e55b2e7288a53219b2abf3f8
Matthew Rice <matthewcrice32@gmail.com>
2017-03-04 17:50:56 -0500

    added package installer and sample nmap

--- setup.sh
+++ setup.sh
@@ -5,3 +5,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cp $DIR/.bashrc ~/
 cp $DIR/.vimrc ~/
+
+# Only works for mac and ubuntu for now
+OS_IDENTIFIER=`uname`
+
+# Update packages
+
+if [ "$OS_IDENTIFIER" == "Darwin" ]; then
+	echo "Mac Detected"
+	port selfupdate
+	port upgrade outdated
+	port install nmap
+else
+    echo "Linux (Ubuntu) detected"
+    apt-get update
+    apt-get upgrade
+    apt-get install nmap
+fi
