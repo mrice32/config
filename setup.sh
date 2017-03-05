1e008eb37a6de4dccb954cb8cfb800ef2ef22678
Matthew Rice <matthewcrice32@gmail.com>
2017-03-05 15:05:49 -0500

    corrected bash prompt pull to update and added .bash_profile

--- setup.sh
+++ setup.sh
@@ -7,10 +7,14 @@ USER_NAME=$(printf '%s' "${SUDO_USER:-$USER}")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Non sudo commands
+sudo -u $USER_NAME cp $DIR/.bash_profile ~/
 sudo -u $USER_NAME cp $DIR/.bashrc ~/
 sudo -u $USER_NAME cp $DIR/.vimrc ~/
 
-sudo -u $USER_NAME git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
+# Move so that the clone will work initially, and then overwrite whatever currently exists if anything
+INITIAL_DIR_NAME=~/bash-git-prompt
+sudo -u $USER_NAME git clone https://github.com/magicmonty/bash-git-prompt.git $INITIAL_DIR_NAME
+sudo -u $USER_NAME mv $INITIAL_DIR_NAME ~/.bash-git-prompt
 
 # Only works for mac and ubuntu for now
 OS_IDENTIFIER=`uname`
